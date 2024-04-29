2050d146103bb57d937e566c47f07704301d860b
Guillaume Flambard <g.flambard@gmail.com>
2024-04-29 20:18:18 +0200

    update the background doc

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,35 +1,40 @@
-// background.ts
+// Interface to define the structure of data stored in chrome.storage
 interface StorageData {
   totalDataReceived: number;
   totalEnergyConsumed: number;
   totalCo2Emissions: number;
 }
 
-function bytesToMegabytes(bytes: number): number {
-  return bytes / 1000000; // converting bytes to MB
-}
+// Converts bytes to megabytes for easier interpretation
+const bytesToMegabytes = (bytes: number): number => bytes / 1000000;
 
-function calculateEnergy(bytes: number): number {
-  // Assuming 0.2 kWh per GB, thus 0.0002 kWh per MB
+// Calculates energy consumption in kWh from the number of bytes transferred
+const calculateEnergy = (bytes: number): number => {
+  // Assuming 0.2 kWh energy consumption per GB transferred
   return bytesToMegabytes(bytes) * 0.0002;
-}
+};
 
-function calculateCO2(bytes: number): number {
-  // Assuming 0.1 kg CO2 per GB, thus 0.0001 kg per MB
+// Calculates CO2 emissions in kilograms from the number of bytes transferred
+const calculateCO2 = (bytes: number): number => {
+  // Assuming 0.1 kg CO2 emissions per GB transferred
   return bytesToMegabytes(bytes) * 0.0001;
-}
+};
 
+// Listener for incoming HTTP responses
 chrome.webRequest.onHeadersReceived.addListener(
-  function (details) {
-    const size = details.responseHeaders?.find(
+  (details) => {
+    // Extract the 'Content-Length' header, which indicates the size of the response body
+    const contentLengthHeader = details.responseHeaders?.find(
       (header) => header.name.toLowerCase() === "content-length"
-    )?.value;
-    const dataSize = parseInt(size || "0", 10);
+    );
+    const dataSize = parseInt(contentLengthHeader?.value || "0", 10);
 
     if (dataSize > 0) {
+      // Retrieve current totals from storage
       chrome.storage.local.get(
         ["totalDataReceived", "totalEnergyConsumed", "totalCo2Emissions"],
         (result: Partial<StorageData>) => {
+          // Calculate new totals
           const newTotalDataReceived =
             (result.totalDataReceived || 0) + dataSize;
           const newTotalEnergyConsumed =
@@ -37,6 +42,7 @@ chrome.webRequest.onHeadersReceived.addListener(
           const newTotalCo2Emissions =
             (result.totalCo2Emissions || 0) + calculateCO2(dataSize);
 
+          // Update storage with new values
           chrome.storage.local.set({
             totalDataReceived: newTotalDataReceived,
             totalEnergyConsumed: newTotalEnergyConsumed,
@@ -46,6 +52,6 @@ chrome.webRequest.onHeadersReceived.addListener(
       );
     }
   },
-  { urls: ["<all_urls>"] },
+  { urls: ["<all_urls>"] }, // Specifies that the listener should observe all URLs
   ["responseHeaders"]
 );
